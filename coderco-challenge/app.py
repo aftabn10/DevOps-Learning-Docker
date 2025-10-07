41bb289ea3d3bf5e62077a0b1ac9b56b2b912673
aftabn10 <aftab@mediadezines.co.uk>
2025-10-07 17:38:25 +0100

    Added HTML and CSS to Homepage

--- coderco-challenge/app.py
+++ coderco-challenge/app.py
@@ -10,9 +10,69 @@ r = redis.Redis(host=redis_host, port=redis_port)
 @app.route("/")
 def welcome():
     return """
-    Welcome Page
-    <br>
-    <a href="/count">Click here to see the visit count!</a>
+    <html>
+    <head>
+    <style>
+            body {
+                background-color: #919682;
+                font-family: Arial, sans-serif;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+            }
+            .container {
+                text-align: center;
+                color: white;
+            }
+            h1 {
+                font-weight: bold;
+                margin-bottom: 20px;
+            }
+             p {
+                font-size: 14px;
+                margin-bottom: 40px;
+                font-weight:600;
+                color: #f4f3f1;
+            }
+            .button-container {
+                display: flex;
+                justify-content: center;
+                gap: 20px;
+            }
+            a.button {
+                display: inline-block;
+                padding: 15px 30px;
+                font-size: 16px;
+                color: white;
+                background-color: #68756D;
+                border: 2px solid #f4f3f1;
+                border-radius: 8px;
+                text-decoration: none;
+                text-transform: capitalize;
+                transition: all 0.2s ease-in-out;
+            }
+            a.button:hover {
+                font-weight:bold;
+            }
+            a.button:active {
+                background-color: #68756D;
+
+            }
+        </style>
+    </head>
+    <body>
+    <div class="container">
+            <h1>MediaDezines: EazyTracker</h1>
+            <p>Track how many times this page has been visited and learn more about the challenge below.</p>
+            <div class="button-container">
+                <a class="button" href="/count">View Count</a>
+                <a class="button" href="/about">About</a>
+            </div>
+        </div>
+    </body>
+    </html>
     """
 
 @app.route("/count")
@@ -24,6 +84,11 @@ def count():
     <a href="/">Go back to Homepage</a>
     """
 
+@app.route("/about")
+def about():
+    return f"""
+    About Us Page
+    """
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002)
